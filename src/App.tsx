bc3f9ddb28776802d1911165dcfc84a5478cdf03
shahyn <shahyn@wiris.com>
2020-11-14 13:35:16 +0100

    breaking change: added typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Unsplash from 'unsplash-js';
+
+// TODO: Replace "APP_ACCESS_KEY" with your own key, which
+// can be generated here: https://unsplash.com/developers
+// const unsplash = new Unsplash({ accessKey: 'APP_ACCESS_KEY' });
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
